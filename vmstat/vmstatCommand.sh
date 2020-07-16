9ac8bf8a68afdf786499db3dc6183e021997d7c1
orzohmygodorz <orzohmygodorz@gmail.com>
2020-07-16 12:04:33 +0800

    check cpu-bound analysis

--- vmstat/vmstatCommand.sh
+++ vmstat/vmstatCommand.sh
@@ -10,10 +10,15 @@ declare -a vmstatArray_alarm
 for ((i=0; i<17; i++)); do vmstatArray_alarm+=("0"); done
 
 #
+# alarm boundary, here we could see as last duration
+#
+alarm_boundary=4
+
+#
 # Loop monitor process with vmstat command
 #
 monitor_start() {
-    # the first result of vmstat is not accurate
+    # The first line shows the statistics since the system was started
     mapfile -t vmstatArray < <( vmstat 1 2 )
     if [[ ${#vmstatArray_old[@]} == "0" ]]; then
         vmstatArray_old=( $( echo ${vmstatArray[-2]}) )
@@ -30,7 +35,7 @@ monitor_start() {
     do 
         vmstatArray_delta+=($(( ${vmstatArray_new[$i]} - ${vmstatArray_old[$i]} )))
     done
-    echo ${vmstatArray_delta[@]}
+    echo "vmstatArray_delta: " ${vmstatArray_delta[@]}
 
     vmstatArray_delta_level=()
     for i in "${!vmstatArray_old[@]}"
@@ -41,21 +46,37 @@ monitor_start() {
         fi
         vmstatArray_delta_level+=($((${vmstatArray_delta[$i]} / $base)))
     done
-    echo ${vmstatArray_delta_level[@]}
+    echo "vmstatArray_delta_level: " ${vmstatArray_delta_level[@]}
 }
 
 #
 # Alarm
 #
+# procs_r_b
+r_b_compare_alarm() {
+    if [[ ${vmstatArray_new[0]} -gt ${vmstatArray_new[1]} ]]; then
+        (( vmstatArray_alarm[0]++ ))
+    elif [[ ${vmstatArray_new[0]} -lt ${vmstatArray_new[1]} ]]; then
+        (( vmstatArray_alarm[1]++ ))
+    fi     
+}
+# io_bo (write blocks / per second)
+bo_alarm() {
+    if [[ ${vmstatArray_new[9]} -gt 100000 ]]; then
+        (( vmstatArray_alarm[9]++ ))
+    fi    
+}
 # system_in (interupt)
-#in_alarm() {
-#    
-#}
+in_alarm() {
+    if [[ ${vmstatArray_new[10]} -gt 100000 ]]; then
+        (( vmstatArray_alarm[10]++ ))
+    fi    
+}
 # system_cs (content switch)
 # Reference: https://books.google.com.tw/books?id=IagfgRiKWd4C&pg=PA442&lpg=PA442&dq=System+diagnosis+cpu+mem+io+bound&source=bl&ots=w_i3o2SuAi&sig=ACfU3U3ZV2YMe5Kpw_V5yTk-uyOZWPNKag&hl=zh-TW&sa=X&ved=2ahUKEwjun43M4a3qAhUawosBHe8uBKIQ6AEwAHoECAsQAQ#v=onepage&q=System%20diagnosis%20cpu%20mem%20io%20bound&f=false
 cs_alarm() {
     if [[ ${vmstatArray_new[11]} -gt 100000 ]]; then
-        vmstatArray_alarm[11]=1
+        (( vmstatArray_alarm[11]++ ))
     fi
 }
 
@@ -65,7 +86,7 @@ cs_alarm() {
 vmstatArray_alarm_trigger() {
     local alarm=false
     for i in ${!vmstatArray_alarm[@]}; do
-        if [[ ${vmstatArray_alarm[$i]} != "0" ]]; then
+        if [[ ${vmstatArray_alarm[$i]} == $alarm_boundary ]]; then
             alarm=true
             break
         fi
@@ -89,15 +110,34 @@ while [ $monitor_seconds -lt $monitor_time ]; do
     monitor_start
 
     if [[ $monitor_seconds != "0" ]]; then 
+        r_b_compare_alarm
+        bo_alarm
+        in_alarm
         cs_alarm
     fi 
     let monitor_seconds+=1
     sleep 1
+    echo ${vmstatArray_alarm[@]}
 
     if $(vmstatArray_alarm_trigger); then
         break
     fi
 done
 
+#
+# Analysis the bottleneck
+#
+# CPU-bound
+check_cpu_bound() {
+    local cpu_bound=false
+    # in and cs is high
+    if [ ${vmstatArray_alarm[10]} -gt "0" ] && [ ${vmstatArray_alarm[11]} -gt "0" ] && [ ${vmstatArray_alarm[0]} -gt "0" ]; then
+        cpu_bound=true
+    fi
+    echo "$cpu_bound"
+}
+
+if $(check_cpu_bound); then
+    echo "This event is CPU-bound."
+fi
 clean
-echo ${vmstatArray_alarm[@]}
