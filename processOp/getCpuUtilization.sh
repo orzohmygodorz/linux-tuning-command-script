3fa39e45de5b315ce8b90ca1df4a7f10afaf82af
orzohmygodorz <orzohmygodorz@gmail.com>
2020-07-18 16:54:33 +0800

    get cpu utilization impl

--- processOp/getCpuUtilization.sh
+++ processOp/getCpuUtilization.sh
@@ -8,31 +8,44 @@ if [[ "${#argArray[@]}" == "0" ]]; then
     exit 0
 fi
 process_name=${argArray[0]}
+echo "Usage: ./getCpuUtilization.sh <process_name> <cpu_monitor_duration>"
 
 pidArray=$( ./getPid.sh "$process_name" )
 pidArray=( $pidArray )
-
-#
-# 
-# pidstat format: 12:42:36 PM   UID       PID    %usr %system  %guest    %CPU   CPU  Command
 cpu_monitor_duration=3
 if [[ ${argArray[1]} != "" ]]; then cpu_monitor_duration=${argArray[1]}; fi
+pidstatCommand="pidstat 1 1 "
 declare -A cpuUtilizationMatrix
+for ((i=0; i<${#pidArray[@]}; i++)); do
+    pidstatCommand+="-p ${pidArray[$i]} "
+    cpuUtilizationMatrix[$i,0]=${pidArray[$i]}
+done
+
+#
+# CPU Utilization
+# pidstat format: 12:42:36 PM   UID       PID    %usr %system  %guest    %CPU   CPU  Command
+pidstatArray=()
 list_process_cpu_utilization() {
-    for ((i=0; i<${#pidArray[@]}; i++)); do
-        cpuUtilizationMatrix[$i,0]=${pidArray[$i]}
-        mapfile -t pidstatArray < <( pidstat 1 $cpu_monitor_duration -p ${pidArray[$i]} )
-        for ((j=0; j<(($cpu_monitor_duration + 3)); j++)); do
+    for ((i=0; i<$cpu_monitor_duration; i++)); do
+        #cpuUtilizationMatrix[$i,0]=${pidArray[$i]}
+        mapfile -t pidstatArray < <( ${pidstatCommand[@]} )
+        for ((j=0; j<((${#pidArray[@]} + 3)); j++)); do
             if [[ $j -lt "3" ]]; then continue; fi
 
             #echo ${pidstatArray[$j]}
-            #printf "%d %f\n" ${pidArray[$i]} $( echo ${pidstatArray[$j]} | cut -d' ' -f8)
-            #printf "%f " $( echo ${pidstatArray[$j]} | cut -d' ' -f8 )
-            cpuUtilizationMatrix[$i,(( $j - 2 ))]=$( echo ${pidstatArray[$j]} | cut -d' ' -f8 )
-            echo $( echo ${pidstatArray[$j]} | cut -d' ' -f10 )
+            cpuUtilizationMatrix[$(( $j - 3 )),$(( $i + 1 ))]=$( echo ${pidstatArray[$j]} | cut -d' ' -f8 )
+            #echo "${cpuUtilizationMatrix[$(( $i + 1 )),$(( $j - 3 ))]}"
         done
         #cpuUtilizationMatrix[$i,(( 1 + $cpu_monitor_duration + 1 ))]=$( echo ${pidstatArray[$j]} | cut -d' ' -f10 )
-        unset pidstatArray
+    done
+}
+add_command_name() {
+    for ((i=0; i<${#pidArray[@]}; i++)); do
+        if [[ $j -lt "3" ]]; then continue; fi
+
+        cpuUtilizationMatrix[$i,$(( $cpu_monitor_duration + 1 ))]=$( echo ${pidstatArray[$(( $i + 3 ))]} | cut -d' ' -f 10 )
+        #echo $( echo ${pidstatArray[$(( $i + 3 ))]} | cut -d' ' -f 10 )
+        #echo ${cpuUtilizationMatrix[$i,$(( $cpu_monitor_duration + 1 ))]}
     done
 }
 
@@ -40,7 +53,9 @@ list_process_cpu_utilization() {
 # clean
 #
 clean() {
+    unset pidstatArray
     unset cpuUtilizationMatrix
+    unset pidstatCommand
     unset cpu_monitor_duration
     unset pidArray
     unset process_name
@@ -48,11 +63,13 @@ clean() {
 }
 
 list_process_cpu_utilization
+add_command_name
 for ((i=0; i<${#pidArray[@]}; i++)); do
     printf "%d " ${cpuUtilizationMatrix[$i,0]}
     for ((j=0; j<$cpu_monitor_duration; j++)); do
-        printf "%f " ${cpuUtilizationMatrix[$i,(( $j + 1 ))]}
+        #printf "$i,$(( $j + 1 )) "
+        printf "%f " ${cpuUtilizationMatrix[$i,$(( $j + 1 ))]}
     done
-    echo
+    echo ${cpuUtilizationMatrix[$i,$(( $cpu_monitor_duration + 1 ))]}
 done
 clean
